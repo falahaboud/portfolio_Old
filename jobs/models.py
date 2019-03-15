6d4d2fc9f46da85229bda6cb1e035d287bbd77eb
falahaboud <falah.aboud@gmail.com>
2019-03-15 18:34:20 +0100

    Detail seite erstellt und links aktiviert

--- jobs/models.py
+++ jobs/models.py
@@ -5,3 +5,7 @@ class job(models.Model):
 
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
+
